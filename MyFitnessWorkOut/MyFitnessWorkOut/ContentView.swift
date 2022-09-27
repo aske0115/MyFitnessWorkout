c3369c048538aeb324d4cb2e2a8bf5e215bbe095
이근화(Geunhwa Lee)/App개발팀/11ST <burzum17@sk.com>
2022-09-27 21:49:08 +0900

    - TCA example counter

--- MyFitnessWorkOut/MyFitnessWorkOut/ContentView.swift
+++ MyFitnessWorkOut/MyFitnessWorkOut/ContentView.swift
@@ -6,11 +6,64 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct State: Equatable {
+    var counter = 0
+}
+
+enum Action: Equatable {
+    case increase
+    case decrease
+}
+
+struct Environment {
+    //Future Dependencies...
+}
+
+let reducer = Reducer<State, Action, Environment> { state,  action, environment in
+    switch action {
+    case .increase:
+        state.counter += 1
+        return Effect.none
+    case .decrease:
+        state.counter -= 1
+        return Effect.none
+    }
+}
 
 struct ContentView: View {
+    let store: Store<State, Action> = Store(initialState: State(), reducer: reducer, environment: Environment())
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        WithViewStore(self.store) { viewStore in
+            HStack {
+                Button {
+                    viewStore.send(.decrease)
+                } label: {
+                    Text("-")
+                        .padding(10)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .buttonStyle(.plain)
+                Text("\(viewStore.counter)")
+                    .padding(5)
+                Button {
+                    viewStore.send(.increase)
+                } label: {
+                    Text("+")
+                        .padding(10)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .buttonStyle(.plain)
+            }
+            
+            
+        }
     }
 }
 
